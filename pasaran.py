f93ccc612e1b04174b267480bf5952d3d8e0825b
Rony Agung Rahmanto <lantip@gmail.com>
2013-11-24 21:51:02 +0700

    differenciate between day and night

--- pasaran.py
+++ pasaran.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
-from datetime import datetime
+from datetime import datetime, timedelta
 
-datenow     = datetime.today()
+datenow     = datetime.now()
 datebase    = datetime.strptime('1 1 1800', '%d %m %Y')
+tomorrow    = datenow + timedelta(days=1)
 
-#array to get the pasaran
+# array to get the pasaran
 pasaran     = ['Pon', 'Wage', 'Kliwon', 'Legi', 'Pahing']
-#array to get the date name in javanese
+# array to get the date name in javanese
 dino        = ['Minggu', 'Senen', 'Seloso', 'Rebo', 'Kemis', 'Jemuwah', 'Septu']
 
 # find the difference between now and base date, in days
 diff        = datenow - datebase
 diffdays    = diff.days
 
-#find the modulo by 5
+# find the modulo by 5
 
 modulo      = diffdays % 5
 
+# find out wether now is day or night
+dn          = datenow.strftime("%p")
+hn          = int(datenow.strftime("%I"))
 
-print "Saiki dino " + dino[int(datenow.strftime("%w"))]+" "+ pasaran[modulo]
+if dn == "PM" and hn > 5:
+    diff        = tomorrow - datebase
+    diffdays    = diff.days
+    modulo      = diffdays % 5
+    print "Saiki malem " + dino[int(tomorrow.strftime("%w"))]+" "+pasaran[modulo]
+else:
+    print "Saiki dino " + dino[int(datenow.strftime("%w"))]+" "+ pasaran[modulo]
